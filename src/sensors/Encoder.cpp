a6199149d9227fe2bdf53d21f8b6ca876f9b070d
gagarcr <ggarciaspam@gmail.com>
2022-03-09 01:26:45 +0100

    Clean

--- src/sensors/Encoder.cpp
+++ src/sensors/Encoder.cpp
@@ -173,9 +173,7 @@ int Encoder::needsSearch(){
 
 // private function used to determine if encoder has index
 int Encoder::hasIndex(){
-  //TODO: fix
-  return 0;
-  //return index_pin != 0;
+  return index_pin != 0;
 }
 
 
